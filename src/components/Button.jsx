f0865532f277a8ef7920cd57ef401432bdd35a93
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-03 22:04:20 +0530

    git commit -m "added props"

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 
 import { IoIosReturnRight } from "react-icons/io";
 
-const Button = () => {
+const Button = ({name}) => {
     return (
         <div className="min-w-40 px-3 py-2 flex items-center justify-between bg-zinc-100 text-black rounded-full ">
-            <span className='text-sm '>Get Started</span>
+            <span className='text-sm '>{name}</span>
             <IoIosReturnRight/>
         </div>
     )
