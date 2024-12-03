1ffcb6d32d1928bd1d37634852788fdf57d2b10c
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-03 16:35:37 +0530

    git commit -m "Added Navbar component to App with custom styling and layout "

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Navbar from "./components/Navbar.jsx";
+
+
 const App = () => {
     return (
-        <div className="">App</div>
+        <div className="w-full h-screen bg-zinc-900 font-satoshi_variable text-white">
+            <Navbar />
+        </div>
     )
 }
 export default App
