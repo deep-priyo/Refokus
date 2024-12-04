ac20d8b39c690cf20d4b93531a850c9768660360
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-04 20:31:45 +0530

    git commit -m "styling changes"

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { IoIosReturnRight } from "react-icons/io";
 
 const Button = ({name}) => {
     return (
-        <div className="min-w-40 px-3 py-2 flex items-center justify-between bg-zinc-100 text-black rounded-full ">
+        <div className="w-40 px-3 py-2 flex items-center justify-between bg-zinc-100 text-black rounded-full font-satoshi_variable ">
             <span className='text-sm '>{name}</span>
             <IoIosReturnRight/>
         </div>
