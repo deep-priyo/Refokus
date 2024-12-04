a480dcf688b55903b30897a763d45a6782105196
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-04 20:35:48 +0530

    git commit -m "Enhance App: Integrate Navbar, Work, Stripes, Products, and Marquees"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Navbar from "./components/Navbar.jsx";
+import Work from "./components/Work.jsx";
+import Stripes from "./components/Stripes.jsx";
+import Products from "./components/Products.jsx";
+import Marquees from "./components/Marquees.jsx";
+
 
 
 const App = () => {
     return (
-        <div className="w-full h-screen bg-zinc-900 font-satoshi_variable text-white">
+        <div className="w-full h-full bg-zinc-900 font-satoshi text-white">
             <Navbar />
+            <Work/>
+            <Stripes/>
+            <Products/>
+            <Marquees/>
         </div>
     )
 }
