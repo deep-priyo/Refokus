00df77a35fabb51d2e7cf7c66e028cdd0550ce4b
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-04 22:30:07 +0530

    git commit -m " Enhance Cards Component: Flexible Layout and Interactive Styles"

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,8 +3,12 @@ import Card from "./Card.jsx";
 
 const Cards = () => {
     return (
-        <div>
-            <Card/>
+        <div className="w-full">
+           <div className="max-w-screen-xl mx-auto py-20 flex gap-3">
+               <Card width={"basis-1/3"} start={false} para={true} />
+               <Card width={"basis-2/3"} start={true} para={false} hover={"hover:bg-violet-500"}/>
+           </div>
+
         </div>
     )
 }
