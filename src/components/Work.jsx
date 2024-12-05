251b401338fbfad28ad75bc22d0db542d65fbf1f
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-05 14:08:15 +0530

    git commit -m " Add animations for image visibility based on scroll progress in Work component"

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,13 @@
 import {useState} from 'react'
-
+import {motion, useMotionValueEvent} from "motion/react"
+import { useScroll } from "motion/react"
 const Work = () => {
     const [images, setImages] = useState([
         {
             url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
             top: "50%",
             left: "50%",
-            isActive: true,
+            isActive: false,
         },
         {
             url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
@@ -39,6 +40,41 @@ const Work = () => {
             isActive: false,
         },
     ]);
+    const { scrollYProgress } = useScroll()
+    scrollYProgress.on("change",(data) => {
+        function imagesShow(arr)
+        {
+            setImages(prev =>(
+                prev.map((item,index)=>(arr.indexOf(index))===-1 ? {...item,isActive: false } : {...item,isActive: true})
+            ))
+        }
+        switch (Math.floor(data*100))
+        {
+            case 0:
+                console.log("1")
+                imagesShow([])
+                break;
+            case 1:
+                imagesShow([0])
+                break;
+            case 2:
+                imagesShow([0,1])
+                break;
+            case 3:
+                imagesShow([0,1,2])
+                break;
+            case 4:
+                imagesShow([0,1,2,3])
+                break;
+            case 5:
+                imagesShow([0,1,2,3,4])
+                break;
+            case 6:
+                imagesShow([0,1,2,3,4,5])
+                break;
+        }
+    })
+
     return (
         <div className="w-full mt-8  ">
             <div className="relative max-w-screen-xl mx-auto text-center">
@@ -47,7 +83,7 @@ const Work = () => {
                 </h1>
                 <div className='absolute w-full h-full top-0 '>
                     {images.map((image, i) => (image.isActive &&
-                        (<img className="absolute w-60 rounded-lg -translate-x-[50%] -translate-y-[50%]" src={image.url} key={i} alt="" style={{top: image.top, left: image.left}} />)
+                        (<img className="absolute w-80 rounded-xl -translate-x-[50%] -translate-y-[50%]" src={image.url} key={i} alt="" style={{top: image.top, left: image.left}} />)
                     ))}
                 </div>
             </div>
