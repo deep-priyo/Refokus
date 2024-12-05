225d6bf93f2a38ccb87a7b53519c460ddd32cf32
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-05 21:02:15 +0530

    git commit -m " minor changes"

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import Card from "./Card.jsx";
 
 const Cards = () => {
@@ -6,7 +6,7 @@ const Cards = () => {
         <div className="w-full">
            <div className="max-w-screen-xl mx-auto py-20 flex gap-3">
                <Card width={"basis-1/3"} start={false} para={true} />
-               <Card width={"basis-2/3"} start={true} para={false} hover={"hover:bg-violet-500"}/>
+               <Card width={"basis-2/3"} start={true} para={false} hover={"hover:bg-violet-500 transition duration-600 ease-in-out"}/>
            </div>
 
         </div>
