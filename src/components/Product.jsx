8060f039b42c39bceb2a08749fac020fbc9ed4f9
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-05 21:05:22 +0530

    git commit -m " Enhance Product Component with Hover Interactions and Dynamic Buttons"

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import { useState } from 'react';
 import Button from "./Button.jsx";
 
+const Product = ({ title, description, live, casee, mover, count }) => {
+    const [buttonVisibility, setButtonVisibility] = useState(false);
 
-const Product = ({title,description,live,casee}) => {
     return (
-        <div className="w-full py-20 ">
-            <div className="max-w-screen-xl mx-auto flex items-center justify-between">
-               <h1 className='text-6xl capitalize font-semibold'>{title}</h1>
-                <div className='details w-1/3'>
+        <div className="w-full py-20 h-[23rem]">
+            <div
+                className="max-w-screen-xl mx-auto flex items-center justify-between"
+                onMouseEnter={() => {
+                    setButtonVisibility(true);
+                    mover(count);
+                }}
+                onMouseLeave={() => setButtonVisibility(false)}
+            >
+                <h1 className="text-6xl capitalize font-semibold">{title}</h1>
+                <div className="details w-1/3">
                     <p className="mb-10">{description}</p>
                     <div className="flex items-center gap-5">
-                        {live && (<Button name={"Live Project"}/>)}
-                        {casee && (<Button name={"Case Project"}/>)}
+                        {live && (
+                            <div
+                                className={`transition-opacity duration-500 ${
+                                    buttonVisibility ? 'opacity-100' : 'opacity-0'
+                                }`}
+                            >
+                                <Button name={"Live Project"} />
+                            </div>
+                        )}
+                        {casee && (
+                            <div
+                                className={`transition-opacity duration-500 ${
+                                    buttonVisibility ? 'opacity-100' : 'opacity-0'
+                                }`}
+                            >
+                                <Button name={"Case Project"} />
+                            </div>
+                        )}
+                    </div>
                 </div>
-
             </div>
         </div>
-        </div>
-    )
-}
-export default Product
+    );
+};
+
+export default Product;
