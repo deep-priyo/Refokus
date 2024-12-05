71a6ea97b8d468535b8d0d717f3491beedd68f07
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-12-05 21:00:37 +0530

    git commit -m " added hover animation"

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,48 @@
-
 import { IoIosReturnRight } from "react-icons/io";
+import { motion } from "framer-motion";
 
-const Button = ({name}) => {
+const Button = ({ name }) => {
     return (
-        <div className="w-40 px-3 py-2 flex items-center justify-between bg-zinc-100 text-black rounded-full font-satoshi_variable ">
-            <span className='text-sm '>{name}</span>
-            <IoIosReturnRight/>
-        </div>
-    )
-}
-export default Button
+        <motion.div
+            className="w-40 px-3 py-2 flex items-center justify-between bg-zinc-100 text-black rounded-full font-satoshi_variable"
+            whileHover={{
+                rotateX: 360,
+                transition: { duration: 0.8, ease: "easeInOut" },
+            }}
+        >
+            <motion.span
+                initial={{ y: 0 }}
+                whileHover={{
+                    y: 20, // Fall back
+                    opacity: 0,
+                    transition: { duration: 0.3 },
+                }}
+                animate={{
+                    y: 0, // Moves back
+                    opacity: 1,
+                    transition: { delay: 0.3, duration: 0.5 },
+                }}
+                className="text-sm"
+            >
+                {name}
+            </motion.span>
+            <motion.div
+                initial={{ y: 0 }}
+                whileHover={{
+                    y: 20,
+                    opacity: 0,
+                    transition: { duration: 0.3 },
+                }}
+                animate={{
+                    y: 0,
+                    opacity: 1,
+                    transition: { delay: 0.3, duration: 0.5 },
+                }}
+            >
+                <IoIosReturnRight />
+            </motion.div>
+        </motion.div>
+    );
+};
+
+export default Button;
